b993d9f06251e830adcf71ab61e2d0643a3b5a0d
ZWGroce <47144376+ZWGroce@users.noreply.github.com>
2019-04-17 06:42:59 -0400

    Self-Balancing Update V1
    Adds working code for self-balancing without battery pack on top

--- balancing/balancing.ino
+++ balancing/balancing.ino
@@ -7,8 +7,8 @@
 #include <Simple_PID.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60);
-Adafruit_StepperMotor *leftStep = AFMS.getStepper(200,1);
-Adafruit_StepperMotor *rightStep = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *leftStep = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *rightStep = AFMS.getStepper(200, 1);
 
 void forwardStepLeft(){
   leftStep->onestep(FORWARD, DOUBLE);  
@@ -31,7 +31,7 @@ AccelStepper rightStepper(forwardStepRight, backwardStepRight);
 
 //*****Define Variables*****
 int start = 0;
-double rpm = 200.0;
+double motorSpeed = 0.0;
 
 //MPU-6050 Variables
 int ax, ay, az, gx, gy, gz;
@@ -49,22 +49,28 @@ double alpha = 0.1;
 FOLPF filter = FOLPF(alpha);
 
 //PID
-double kp = 2.5, ki = 0.0, kd = 0.0;
+double kp = 10.0, ki = 0.0, kd = 1.0;
 double uleft, uright, du;
-double duMax = 100.0;
+double duMax = 15.0, uMax = 100.0;
 double lastAngle;
 Simple_PID controller = Simple_PID(kp, ki, kd);
 
 //*****Program Initialization*****
 void setup() {
   Wire.begin();
+  AFMS.begin();
   sensor.initialize();
   Serial.begin(9600);
+  //TWBR = 12;
 
-  leftStepper.setMaxSpeed(rpm);
-  leftStepper.setSpeed(rpm);
-  rightStepper.setMaxSpeed(rpm);
-  rightStepper.setSpeed(rpm);
+  leftStepper.setMaxSpeed(100.0);
+  leftStepper.setSpeed(0.0);
+  //leftStepper.setAcceleration(1000.0);
+  //leftStepper.moveTo(0);
+  rightStepper.setMaxSpeed(100.0);
+  rightStepper.setSpeed(0.0);
+  //rightStepper.setAcceleration(1000.0);
+  //rightStepper.moveTo(0);
 
   sensor.setFullScaleAccelRange(0);  // +/- 2g
   sensor.setFullScaleGyroRange(0);  // +/- 250 deg/s
@@ -105,6 +111,9 @@ void loop() {
   rollA = (atan2(-ayf, azf) * 180 / M_PI) - rollAccelBias;  //Eq.2 of lab manual page 137
   pitchA = (atan2(axf, sqrt((ayf * ayf) + (azf * azf))) * 180 / M_PI) - pitchAccelBias;  //Eq.1
 
+  //rollA = (atan2(-ayf, azf) * 180 / M_PI);  //Eq.2 of lab manual page 137
+  //pitchA = (atan2(axf, sqrt((ayf * ayf) + (azf * azf))) * 180 / M_PI);  //Eq.1
+
   t = millis();
   dt = t - tlast;
   
@@ -115,33 +124,46 @@ void loop() {
     gy -= gyb;
     gz -= gzb;
 
-    rollG += (gx / gyroScale) * (ts/1000);
-    pitchG += (gy / gyroScale) * (ts/1000);
+    rollG += (gx / gyroScale) * (ts/1000.0);
+    pitchG += (gy / gyroScale) * (ts/1000.0);
 
     roll = (0.9996 * rollG) + (0.0004 * rollA);
-    pitch = ((0.9996 * pitchG) + (0.0004 * pitchA)) + 27;
+    pitch = ((0.9996 * pitchG) + (0.0004 * -1.0 * pitchA));
+    pitch *= -1.0;
+    
+    if(pitch < 0){
+      pitch = 0;  
+    }
 
-    if(pitch > -0.5 && pitch > 0.5) start = 1;
+    if(pitch > 27.5 && pitch < 29.5){
+      start = 1;
+    }
 
-    //Serial.print(roll); Serial.print("\t"); Serial.println(pitch);
-    //Serial.print(rollA); Serial.print("\t"); Serial.println(pitchA); Serial.println();
+    //Serial.println(pitch);
+    //Serial.print(pitch); Serial.print("\t"); Serial.print(pitchG); Serial.print("\t"); Serial.println(pitchA);
+    //Serial.print(rollA); Serial.print("\t"); Serial.println(pitchA); //Serial.println();
+    
     if(start == 1){
-      du = controller.getDU(0.0, pitch, t, tlast, lastAngle);
+      du = controller.getDU(29.25, pitch, t, tlast, lastAngle);
       //Serial.println(du);
       du = constrain(du, -duMax, duMax);
       lastAngle = pitch;
-      uleft = leftStepper.currentPosition() + du;
-      uright = rightStepper.currentPosition() + du;
+      motorSpeed += du;
+      //Serial.println(motorSpeed);
+      uleft = -motorSpeed;
+      uleft = constrain(uleft, -uMax, uMax);
+      uright = motorSpeed;
+      uright = constrain(uright, -uMax, uMax);
 
-      /*leftStepper.moveTo(uleft);
-      leftStepper.setSpeed(rpm);
-      rightStepper.moveTo(uright);
-      rightStepper.setSpeed(rpm);*/
+      leftStepper.setSpeed(uleft);
+      rightStepper.setSpeed(uright);
     }
-    
+
     tlast = t;
   }
   
-  //leftStepper.runSpeedToPosition();
-  //rightStepper.runSpeedToPosition();
+  if(start == 1){
+    leftStepper.runSpeed();
+    rightStepper.runSpeed();
+  }
 }
--- stepper_test/stepper_test.ino
+++ stepper_test/stepper_test.ino
@@ -5,8 +5,8 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-Adafruit_StepperMotor *leftStep = AFMS.getStepper(200, 1);
-Adafruit_StepperMotor *rightStep = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *leftStep = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *rightStep = AFMS.getStepper(200, 1);
 
 void forwardStepLeft(){
   leftStep->onestep(FORWARD, DOUBLE);  
@@ -29,31 +29,35 @@ AccelStepper rightStepper(forwardStepRight, backwardStepRight);
 
 void setup() {
   AFMS.begin();
+  TWBR = 12;
+  //Serial.begin(9600);
 
-  leftStepper.setMaxSpeed(200.0);
-  //leftStepper.setAcceleration(100.0);
-  leftStepper.moveTo(30.0);
-  leftStepper.setSpeed(200.0);
+  leftStepper.setMaxSpeed(1000.0);
+  //leftStepper.setAcceleration(1000.0);
+  //leftStepper.moveTo(-50.0);
+  leftStepper.setSpeed(-100.0);
 
-  rightStepper.setMaxSpeed(200.0);
-  //rightStepper.setAcceleration(100.0);
-  rightStepper.moveTo(-30.0);
-  rightStepper.setSpeed(200.0);
+  rightStepper.setMaxSpeed(1000.0);
+  //rightStepper.setAcceleration(1000.0);
+  //rightStepper.moveTo(50.0);
+  rightStepper.setSpeed(100.0);
 
-  TWBR = 12;  //Changes the i2c clock to 400KHz
+  //TWBR = 12;  //Changes the i2c clock to 400KHz
 }
 
 void loop() {
-  if(leftStepper.distanceToGo() == 0){
+  /*if(leftStepper.distanceToGo() == 0){
     leftStepper.moveTo(-leftStepper.currentPosition());
-    leftStepper.setSpeed(200.0); 
+    //leftStepper.setSpeed(500.0); 
   }
 
   if(rightStepper.distanceToGo() == 0){
     rightStepper.moveTo(-rightStepper.currentPosition());
-    rightStepper.setSpeed(200.0); 
-  }
+    //rightStepper.setSpeed(500.0); 
+  }*/
 
-  leftStepper.runSpeedToPosition();
-  rightStepper.runSpeedToPosition();
+  leftStepper.runSpeed();
+  rightStepper.runSpeed();
+
+  //Serial.print(leftStepper.currentPosition()); Serial.print("\t"); Serial.println(rightStepper.currentPosition());
 }
